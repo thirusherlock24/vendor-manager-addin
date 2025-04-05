a48bdef08aaa3f8f95a7e7d856f7ff1a6ed57f61
Thirunavukarasu L <thirusherlock@gmail.com>
2025-04-06 01:10:20 +0530

    config changes

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = async (env, options) => {
       commands: "./src/commands/commands.ts",
     },
     output: {
+      path: __dirname + '/dist', 
       clean: true,
     },
     resolve: {
