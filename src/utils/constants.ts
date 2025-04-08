fb8a1bc73cb9f6e6b907be7e685c353583b7ca56
Thirunavukarasu L <thirusherlock@gmail.com>
2025-04-08 11:15:22 +0530

    password change

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const LOCAL_USERS = [
     {
       username: "admin",
-      passwordHash: "c0e2d3f6b6c7e1c1e4f3f4c8d9e5f8e4f3c2d1b6e7f4c3d2e1f8e5c7d9f6e3"
+      passwordHash: "01039a859a590c59de9cc5dc73cd1a576aa0014e35fd61857f70b86bab6de4fc"
     }
   ];
 
